48f835f53b2ab00558c6629fcc8a8bfaf754fc1e
Georgia Davis <gyang925@gmail.com>
2016-10-25 18:35:18 -0700

    Create studentfile.py

---
+++ studentfile.py
@@ -0,0 +1,34 @@
+# Step 1: Stock
+
+
+
+
+
+# Step 2: Updating Stock
+
+
+
+
+
+# Step 3: Customer Order from Stock
+
+
+
+
+
+
+# Step 4: Inventory
+
+
+
+
+
+
+# Step 5: Customer Order from Inventory
+
+
+
+
+
+
+# Step 6: Remove Items from Inventory
